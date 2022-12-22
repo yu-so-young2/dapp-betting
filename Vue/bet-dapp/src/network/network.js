b2eb54891ea971805be293af041e93b21bb1d231
yu-so-young2 <sy980911@gmail.com>
2022-12-22 15:34:46 +0900

    network.js 작성

---
+++ Vue/bet-dapp/src/network/network.js
@@ -0,0 +1,11 @@
+// 네트워크 설정
+// 메타마스크와 연동되는 provider에 맞는 네트워크 이름을 표시하기 위한 상수 모음
+export const NETWORKS = {
+  1: "Main Net",
+  2: "Deprecated Morden test network",
+  3: "Ropsten test network",
+  4: "Rinkeby test network",
+  42: "Kovan test Nnetworket",
+  4447: "Truffle Develop network",
+  5777: "Ganache Blockchain",
+};
